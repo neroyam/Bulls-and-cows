60459a9da3b717a3e274e45fd8a6a1e8baed0ea0
neroyam <51530174+neroyam@users.noreply.github.com>
2019-06-30 14:30:08 +0300

    Add files via upload
    версия релиз с комментариями основного кода

---
+++ Practic/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practic
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu.Close();
+        }
+        public MediaPlayer beep = new MediaPlayer();
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            HelpWindow helpWindow = new HelpWindow();
+            helpWindow.Show();
+            beep.Open(new Uri(@"D:\Practic\Practic\Resources\boop.mp3", UriKind.Absolute));
+            beep.Play();
+        }
+
+  
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+          GameWindow gameWindow = new GameWindow();
+            gameWindow.Show();
+            this.Close();
+           
+            beep.Open(new Uri(@"D:\Practic\Practic\Resources\boop.mp3", UriKind.Absolute));
+            beep.Play();
+        }
+
+        private void MainMenu_Loaded(object sender, RoutedEventArgs e)
+        { 
+        }
+    }
+}
